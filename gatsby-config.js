e44201af240d57b1bd27931414321b9d50cc940d
Adnan Rafiq <Adnan.Rafiq@cengage.com>
2020-08-12 22:39:26 -0400

    ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,30 @@ module.exports = {
         path: `${__dirname}/content/assets`,
       },
     },
+g    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-173098616-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        
+        // Defers execution of google analytics script after page load
+        defer: true,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "adnanrafiq.com",
+      },
+    },
   ],
   // Customize your site metadata:
   siteMetadata: {
